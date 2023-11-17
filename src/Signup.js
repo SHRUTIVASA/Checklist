e3cc5da7383ee8f361063b31e3158cd297d5c354
Shruti Vasa <shrutivasa07@gmail.com>
2023-11-17 13:35:55 +0530

    forgot password changes

--- src/Signup.js
+++ src/Signup.js
@@ -221,9 +221,6 @@ export default function Signup() {
                   </Button>
                 </Form>
 
-                <div className="w-100 text-center mt-3">
-                  <Link to="/forgot-password">Forgot Password?</Link>
-                </div>
               </Card.Body>
             </Card>
 
